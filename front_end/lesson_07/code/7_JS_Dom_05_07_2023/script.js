1de970161de61eeee5a39467653138f0f620c448
golderosDR <golderos.spr@gmail.com>
2023-07-10 17:06:51 +0200

    Update script.js

--- front_end/lesson_07/code/7_JS_Dom_05_07_2023/script.js
+++ front_end/lesson_07/code/7_JS_Dom_05_07_2023/script.js
@@ -8,18 +8,46 @@ aa
 */
 // const - константа, переприсваивать зачение запрещено
 // let - переменная, переприсваивать зачение можно
-const t = setInterval(move, 20);
-let pos = 0;
-const box = document.getElementById('box');
 
-// 10 + 'px' => '10px'
-// box.style.left = 10 + 'px';
+let posTop = 0;
+let posLeft = 0;
+const box = document.getElementById("box");
+const t = setInterval(moveRight, 20);
 
-function move() {
-    pos++;
-    if (pos == 150) {
-        clearInterval(t);
-    };
-    box.style.left = pos + 'px';
-    box.style.top = pos + 'px';
+function moveRight() {
+    ++posLeft;
+  if (posLeft == 149) {
+    clearInterval(t);
+    t1 = setInterval(moveDown, 20);
+    clearInterval(t4);
+  }
+  box.style.left = posLeft + "px";
+}
+function moveDown() {
+    ++posTop;
+  if (posTop == 149) {
+    clearInterval(t1);
+    t2 = setInterval(moveLeft, 20);
+
+  }
+
+  box.style.top = posTop + "px";
+}
+function moveLeft() {
+    --posLeft;
+  if (posLeft == 1) {
+    clearInterval(t2);
+    t3 = setInterval(moveUp, 20);
+  }
+
+  box.style.left = posLeft + "px";
+}
+function moveUp() {
+    --posTop;
+  if (posTop == 1) {
+    clearInterval(t3);
+    t4 = setInterval(moveRight, 20);
+  }
+
+  box.style.top = posTop + "px";
 }
